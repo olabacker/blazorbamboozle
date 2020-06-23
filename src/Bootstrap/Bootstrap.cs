d4893fe82e894e74a1c9466f7e3084e529eba5c1
Ola Bäcker <olabacker@gmail.com>
2020-06-23 10:34:06 +0200

    added initial bamboozlecard

---
+++ src/Bootstrap/Bootstrap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorBamboozle.Bootstrap
+{
+
+    public enum ColorName
+    {
+        None,
+        Danger,
+        Warning,
+        Success,
+        Info,
+        Primary,
+        Light,
+        Dark,
+        Secondary
+    }
+
+
+    public static class BootstrapExtensions
+    {
+        public static string ToClass(this ColorName c) => c.ToString().ToLower();
+    }
+}
